28434b9d137386766867daf868e2c9c1fb7d5196
cescriba-2022278 <cescriba-2022278@kinal.edu.gt>
2024-05-28 17:17:44 -0600

    BANK-8 #comment favorite accounts #time 17m

---
+++ src/favoriteAccounts/favoriteAccounts.model.js
@@ -0,0 +1,40 @@
+import { Schema, model } from "mongoose";
+import { L } from "../../i18n/i18n-node.js";
+
+const [ACTIVE, INACTIVE] = ["ACTIVE", "INACTIVE"];
+
+const favoriteAccountsSchema = new Schema({
+  account: {
+    type: Schema.Types.ObjectId,
+    required: [true, L.en.DB_ACCOUNT_REQUIRED()],
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    required: [true, L.en.DB_OWNER_REQUIRED()],
+  },
+  alias: {
+    type: String,
+    unique: true,
+    required: [true, L.en.DB_ALIAS_REQUIRED()],
+  },
+  tp_status: {
+    type: String,
+    required: [true, L.en.DB_TP_STATUS_REQUIRED()],
+    enum: [ACTIVE, INACTIVE],
+    default: ACTIVE,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+    required: [true, L.en.DB_CREATED_AT_REQUIRED()],
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now,
+    required: [true, L.en.DB_UPDATED_AT_REQUIRED()],
+  },
+});
+
+const favoriteAccountsModel = model("FavoriteAccounts", favoriteAccountsSchema);
+
+export default favoriteAccountsModel;
