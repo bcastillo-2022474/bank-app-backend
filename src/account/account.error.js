0b8c4a130242b1f04a85e96e1749261e8f720671
cescriba-2022278 <cescriba-2022278@kinal.edu.gt>
2024-06-03 14:41:33 -0600

    BANK-11 #comment Account endpoints are in the process of being finalized for their respective review #time 2d 3h 10m

---
+++ src/account/account.error.js
@@ -0,0 +1,41 @@
+import { getErrorFromErrors } from "../../utils/http-errors.js";
+import { StatusCodes } from "http-status-codes";
+
+export class AccountAlreadyExist extends Error {
+    constructor(message) {
+      super(message);
+      this.name = "AccountAlreadyExist";
+    }
+  }
+  
+  export class AccountNotFound extends Error {
+    constructor(message) {
+      super(message);
+      this.name = "AccountNotFound";
+    }
+  }
+  
+  export class AccountInvalidValue extends Error {
+    constructor(message) {
+      super(message);
+      this.name = "AccountInvalidValue";
+    }
+  }
+
+
+  const errors = [
+    {
+      code: StatusCodes.NOT_FOUND,
+      classes: [AccountNotFound],
+    },
+    {
+      code: StatusCodes.CONFLICT,
+      classes: [AccountAlreadyExist],
+    },
+    {
+      code: StatusCodes.BAD_REQUEST,
+      classes: [AccountInvalidValue],
+    },
+  ];
+
+export const getAccountError = (error) => getErrorFromErrors(error, errors);
