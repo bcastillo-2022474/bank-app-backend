1056e1402d0d006095fe7c61b2bb9526bf324326
cescriba-2022278 <cescriba-2022278@kinal.edu.gt>
2024-06-04 13:42:23 -0600

    BANK-11 #comment test #time 10m

--- tests/account/create-accounts.endpoint.spec.js
+++ tests/account/create-accounts.endpoint.spec.js
@@ -3,53 +3,67 @@ import request from "supertest";
 import { app } from "../../routes.js";
 
 describe("Create account", () => {
-  describe(`should return ${StatusCodes.BAD_REQUEST} when`, () => {
-    it(`the owner is not provided`, async () => {
-      const response = await request(app)
-        .post("/account")
-        .send({ currency: "USD", balance: 100 });
+  const baseRequest = request(app).post("/account");
+
+  describe("should return BAD_REQUEST when", () => {
+    it("the owner is not provided", async () => {
+      const response = await baseRequest.send({
+        currency: "USD",
+        balance: 100,
+      });
       expect(response.status).toBe(StatusCodes.BAD_REQUEST);
+      expect(response.body.error).toBe("Owner is required");
     });
 
-    it(`the currency is not provided`, async () => {
-      const response = await request(app)
-        .post("/account")
-        .send({ owner: "user_id", balance: 100 });
+    it("the currency is not provided", async () => {
+      const response = await baseRequest.send({
+        owner: "user_id",
+        balance: 100,
+      });
       expect(response.status).toBe(StatusCodes.BAD_REQUEST);
+      expect(response.body.error).toBe("Currency is required");
     });
 
-    it(`the balance is not provided`, async () => {
-      const response = await request(app)
-        .post("/account")
-        .send({ owner: "user_id", currency: "USD" });
+    it("the balance is not provided", async () => {
+      const response = await baseRequest.send({
+        owner: "user_id",
+        currency: "USD",
+      });
       expect(response.status).toBe(StatusCodes.BAD_REQUEST);
+      expect(response.body.error).toBe("Balance is required");
     });
 
-    it(`the balance is not a number`, async () => {
-      const response = await request(app)
-        .post("/account")
-        .send({ owner: "user_id", currency: "USD", balance: "not_a_number" });
+    it("the balance is not a number", async () => {
+      const response = await baseRequest.send({
+        owner: "user_id",
+        currency: "USD",
+        balance: "not_a_number",
+      });
       expect(response.status).toBe(StatusCodes.BAD_REQUEST);
+      expect(response.body.error).toBe("Balance must be a number");
     });
   });
 
-  it("should return 201 when creating an account", async () => {
-    const response = await request(app)
-      .post("/account")
-      .send({ owner: "user_id", currency: "USD", balance: 100 });
+  it("should return CREATED when creating an account", async () => {
+    const response = await baseRequest.send({
+      owner: "user_id",
+      currency: "USD",
+      balance: 100,
+    });
     expect(response.status).toBe(StatusCodes.CREATED);
   });
 
-  it(`should return ${StatusCodes.CONFLICT} when trying to create an account with existing owner and currency`, async () => {
+  it("should return CONFLICT when trying to create an account with existing owner and currency", async () => {
     // First, create an account
-    await request(app)
-      .post("/account")
-      .send({ owner: "user_id", currency: "USD", balance: 100 });
+    await baseRequest.send({ owner: "user_id", currency: "USD", balance: 100 });
 
     // Try to create another account with the same owner and currency
-    const response = await request(app)
-      .post("/account")
-      .send({ owner: "user_id", currency: "USD", balance: 200 });
-    expect(response.status).toBe(StatusCodes.BAD_REQUEST);
+    const response = await baseRequest.send({
+      owner: "user_id",
+      currency: "USD",
+      balance: 200,
+    });
+    expect(response.status).toBe(StatusCodes.CONFLICT);
+    expect(response.body.error).toBe("Account already exists");
   });
 });
