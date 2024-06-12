c9c52ce670eb563ab2ec01836def9529e995657f
jcastillo21 <bcastillo-2022474@kinal.edu.gt>
2024-06-12 10:44:20 -0600

    BANK-23 #comment fixing bug and removing logs for dev #time 30m

--- src/utils/encrypt.js
+++ src/utils/encrypt.js
@@ -1,8 +1,6 @@
 import bcryptjs from "bcryptjs";
-import { logger } from "./logger.js";
 
 export const encrypt = (password) => {
-  logger.info("ENCRIPTTTTTTTTTTT UTILIDAD");
   const salt = bcryptjs.genSaltSync();
   const encryptedPassword = bcryptjs.hashSync(password, salt);
   return encryptedPassword;
