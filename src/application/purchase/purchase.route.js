ea46e6fd2e26271bf08bb26072d0f33b237f1521
jcastillo21 <bcastillo-2022474@kinal.edu.gt>
2024-07-10 13:24:34 -0600

    almost done

--- src/application/purchase/purchase.route.js
+++ src/application/purchase/purchase.route.js
@@ -14,6 +14,7 @@ import {
   getPurchaseByAccount,
   getPurchaseByProduct,
   getPurchaseById,
+  getPurchaseByUserId,
 } from "./purchase.controller.js";
 import Product from "../product/product.model.js";
 import Account from "../account/account.model.js";
