c8814d285eb5e25cf93506057038b737ed9ed38d
jcastillo21 <bcastillo-2022474@kinal.edu.gt>
2024-07-10 10:12:42 -0600

    fixing exchange behaviour

--- src/utils/get-money-exchange-rate.js
+++ src/utils/get-money-exchange-rate.js
@@ -2,8 +2,8 @@ import { logger } from "./logger.js";
 
 export async function getMoneyExchangeRate(currencyOne, currencyTwo) {
   logger.info("Getting money exchange rate");
-  logger.info(`Currency one: ${currencyOne.symbol}`);
-  logger.info(`Currency two: ${currencyTwo.symbol}`);
+  logger.info(`Currency one: ${currencyOne.symbol.toLowercase()}`);
+  logger.info(`Currency two: ${currencyTwo.symbol.toLowercase()}`);
   const value = await fetch(
     `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currencyOne.symbol}.json`,
     // eslint-disable-next-line @joao-cst/enforce-consistent-return-express
